2948a95daaad7dcad7c63b47b4925e1a0c56a14d
Wilsondotzip <58171274+Wilsondotzip@users.noreply.github.com>
2025-06-20 13:10:46 +1000

    Firmware re code
    Improved firmware and refactored to be more maintainable.
    - added better smoothing and results updating to fix any noise issues.
    - faster value checking.
    - clean increments of 2

--- Firmware/main/main.ino
+++ Firmware/main/main.ino
@@ -1,71 +1,65 @@
-int pot1 = A0;
-int pot2 = A1;
-int pot3 = A2;
-int pot4 = A3;
+const int knobCount = 4;
+const uint8_t knobPins[knobCount] = {A0, A1, A2, A3};
 
-
-int pot1Val = 0;
-int pot2Val = 0;
-int pot3Val = 0;
-int pot4Val = 0;
-
-int InitialValue = 0;
-
-int valA = 0;
-int valB = 0;
-int valC = 0;
-
+int knobValues[knobCount] = {0};
+bool knobActive[knobCount] = {false};
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 
-  
-  Serial.begin(9600); 
-  bindToSoftware(pot1);
-  bindToSoftware(pot2);
-  bindToSoftware(pot3);
-  bindToSoftware(pot4);
-
+  for (int i = 0; i < knobCount; i++) {
+    bindToSoftware(knobPins[i], i + 1);
+  }
 }
 
 void loop() {
-pot1Val = checkForUpdate(pot1, pot1Val, 1);
-pot2Val = checkForUpdate(pot2, pot2Val, 2);
-pot3Val = checkForUpdate(pot3, pot3Val, 3);
-pot4Val = checkForUpdate(pot4, pot4Val, 4);
-
-  
-
+  for (int i = 0; i < knobCount; i++) {
+    knobValues[i] = checkKnob(knobPins[i], knobValues[i], i);
+  }
 }
-int checkForUpdate(char pin, char pinval, int ID){
-  if(readAv(pin)== pinval)  {}
-  else{
-  Serial.print(readAv(pin));
+
+int checkKnob(uint8_t pin, int prevVal, int index) {
+  int val = map(readAverage(pin), 0, 1024, 0, 100);
+  if (val > 98) val = 100;
+  if (val < 2) val = 0;
+  int delta = abs(val - prevVal);
+
+  if (!knobActive[index]) {
+    if (delta > 1) {
+      knobActive[index] = true;  // Activate knob
+    }
+  } else {
+    if (delta > 2) {
+      sendUpdate(val, index + 1);
+      return val;
+    } else {
+      knobActive[index] = false; // Deactivate
+    }
+  }
+
+  return prevVal;
+}
+
+void sendUpdate(int val, int ID) {
+  Serial.print(val);
   Serial.print("@");
   Serial.print(ID);
   Serial.print("\n");
-  return readAv(pin);
-
-    }  
-  }
-  
-int readAv(char pin){
-  int i;  
-  int sval = 0;
-
-  for (i = 0; i < 5; i++){
-    sval = sval + map(analogRead(pin), 0, 4064, 0, 100);
-    delay(1);
-  }
-  sval = sval / 5; 
-
-  return sval;
 }
-  
-char bindToSoftware(char pin){
-  InitialValue = map(analogRead(pin), 0, 4064, 0, 100);
-  Serial.print(InitialValue);
-  Serial.print("@1");
-  Serial.print("\n");
 
+int readAverage(uint8_t pin) {
+  long total = 0;
+  for (int i = 0; i < 5; i++) {
+    total += analogRead(pin);
   }
+  return total / 5;
+}
+
+void bindToSoftware(uint8_t pin, int ID) {
+  int initialValue = map(analogRead(pin), 0, 1024, 0, 100);
+  if (initialValue > 98) initialValue = 100;
+  Serial.print(initialValue);
+  Serial.print("@");
+  Serial.print(ID);
+  Serial.print("\n");
+}
